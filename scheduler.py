6a4850a7654cd93284d15305cee0833ed50acd62
unknown <wanggu52@ecf.utoronto.ca>
2020-11-07 21:28:16 -0500

    fix bugs

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 
+UUID2Name = {}
+
 class Task():
     """
     task
@@ -15,6 +17,7 @@ class Task():
         duration (int) : in minute
         """
         self.UUID = hash(name + str(ddl))
+        UUID2Name[self.UUID] = name
         self.name = name
         self.ddl = ddl
         self.weight = weight
@@ -77,4 +80,9 @@ test_ddl = "2020, 11, 08, 10, 00"
 test_tasks = [Task("test1", convert(test_ddl), 0.5, 30)]
 current_time = datetime.now()
 test_available_time = [[current_time, convert(test_ddl)]]
-print(schedule(test_tasks, test_available_time))
+schedule_dict = schedule(test_tasks, test_available_time)
+for task_UUID, schedules in schedule_dict.items():
+    print(UUID2Name[task_UUID], "is schedule between")
+    for start_end in schedules:
+        print("start:", str(start_end[0]))
+        print("end  :", str(start_end[1]))
