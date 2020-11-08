18844af46d98154ea0fe15fb8b5e31fb6b3f4358
LittleDijkstraZ <54603864+LittleDijkstraZ@users.noreply.github.com>
2020-11-08 04:13:10 -0500

    extreme testcase1

--- testcase1.py
+++ testcase1.py
@@ -1,7 +1,7 @@
 from scheduler import*
 
 
-task_dict = {"test1":["2020, 11, 08, 1, 00", 0.5, 30],
+task_dict = {"test1":["2020, 11, 08, 1, 00", 0.9, 60],
         "assignment2":["2020, 11, 08, 11, 00", 0.9, 60],
         "assignment1":["2020, 11, 08, 12, 00", 0.9, 600]
         }
@@ -10,7 +10,7 @@ task_list = []
 for key in task_dict:
     t = task_dict[key]
     t[0] = convert(t[0])
-    task_list.append(Task(key,*t) )    
+    task_list.append(Task(key,*t))    
 
 current_time = convert("2020, 11, 08, 0, 00")
 end_time = max(task_dict.values(),key = lambda x:x[0])[0]
